d001ac5aa20c885ec1ab382303b2c84ab92d8b71
Omri Kovalski <omri.kovalski@gmail.com>
2022-04-28 20:15:40 +0300

    add support for serving from folder

--- express/index.js
+++ express/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello From local host'));
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.listen(4200, () => console.log('Listening on port 4000!'));
